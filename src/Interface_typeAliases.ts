ef23055809236a3ad8f9eefaa8c48f18d16f9873
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-11-19 21:05:49 +0600

    add new feature

--- src/Interface_typeAliases.ts
+++ src/Interface_typeAliases.ts
@@ -1,10 +1,10 @@
-//type aliases (not margeable two same type)
+//TODO type aliases (not margeable two same type)
 type Customer = {
   name: string;
   age: number;
   email: string;
 };
-// duplicate type  alias error
+//!   duplicate type  alias error
 type Customer = {
   address: string;
 };
@@ -17,18 +17,31 @@ interface newCustomer {
   email: string;
 }
 
-//Margable interface example
+//TODO Margable interface example
 interface Person {
   name: string;
 }
 interface Person {
   age: number;
 }
-  
+
 const margePersone: Person = {
   name: "Ehalsur Rahman",
   age: 26,
 };
 
+//TODO extending Interface with example
+//step1 parent type
+interface Animal {
+  name: string;
+}
+//step2 child type
+interface Dog extends Animal {
+  breed: string;
+}
 
-
+const myDog: Dog = {
+  name: "Tomy",
+  breed: "light golden",
+};
+console.log(myDog);
